038af58f13f919f34c5d16f1fb5fa8bc717a9478
fershad <27988517+fershad@users.noreply.github.com>
2024-10-18 22:21:46 +0800

    simple variable to sort images

--- src/_data/images.js
+++ src/_data/images.js
@@ -1,21 +1,68 @@
-const directory = 'static/images';
-import fs from 'fs';
+const directory = "static/images";
+import fs from "fs/promises";
 
-const getImages = async () => {
+/**
+ * @constant {string} sort
+ * @default 'asc'
+ * @description Specifies the sorting order. Can be 'asc' for ascending or 'desc' for descending.
+ */
+const sort = 'asc';
 
-const images = [];
-fs.readdir(directory, (err, files) => {
-    if (err) {
+/**
+ * Reads files from the specified directory and gathers metadata.
+ * 
+ * @async
+ * @function readFiles
+ * @returns {Promise<Array<{file: string, meta: {path: string, birthtime: Date}}>>} A promise that resolves to an array of image objects with metadata.
+ */
+const readFiles = async () => {
+    const images = [];
+    try {
+        const files = await fs.readdir(directory);
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            try {
+                const stats = await fs.stat(`${directory}/${file}`);
+                if (stats.isFile()) {
+                    images.push({
+                        file: file,
+                        meta: {
+                            path: `${directory}/${file}`,
+                            birthtime: stats.birthtime,
+                        },
+                    });
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    } catch (err) {
         console.error(err);
-        return;
+    }
+    return images;
+};
+
+/**
+ * Retrieves and sorts images based on the specified sorting order.
+ * 
+ * @async
+ * @function getImages
+ * @returns {Promise<Array<{file: string, meta: {path: string, birthtime: Date}}>>} A promise that resolves to an array of sorted image objects with metadata.
+ */
+const getImages = async () => {
+    let images = await readFiles();
+
+    if (sort === "asc") {
+        images = images.sort((a, b) => {
+            return a.meta.birthtime - b.meta.birthtime;
+        });
+    } else if (sort === "desc") {
+        images = images.sort((a, b) => {
+            return b.meta.birthtime - a.meta.birthtime;
+        });
     }
 
-    files.forEach(file => {
-        images.push(file);
-    });
-});
-
-return images;
-}
+    return images;
+};
 
 export default getImages;
