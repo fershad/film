adc368de0c977fe710441c07202fff64c7d44ef2
fershad <27988517+fershad@users.noreply.github.com>
2024-10-18 22:54:26 +0800

    update the header text and style

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,8 @@
 const environment = process.env.ELEVENTY_RUN_MODE;
 
 const site = {
-    title: "11ty Starter Gallery",
+    title: "Film",
+    tagline: "Photos by Fershad Irani",
     description: "A description of my site",
     lang: "en",
     baseUrl: environment === "serve" ? "http://localhost:8080" : "https://example.com",
