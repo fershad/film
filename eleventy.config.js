3e4e51f8dab4e8ce72c7257e7b20d91d2a36a917
fershad <27988517+fershad@users.noreply.github.com>
2024-10-18 22:22:00 +0800

    minify css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,24 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import fs from "node:fs/promises";
+import path from "node:path";
+import CleanCSS from "clean-css";
+
+const __dirname = import.meta.dirname;
 
 
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory("src");
 	eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
     eleventyConfig.addPassthroughCopy("static");
+
+    eleventyConfig.addShortcode("minifiedcss", async function (code) {
+		return `<style>
+	  ${await fs
+			.readFile(path.resolve(__dirname, "./static/index.css"))
+			.then((data) => new CleanCSS().minify(data).styles)}
+	</style>`;
+	});
+    
 
     eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// which file extensions to process
