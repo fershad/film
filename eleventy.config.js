70a3554cd436c05dfa382e939ef7858f60ece8a3
fershad <27988517+fershad@users.noreply.github.com>
2024-10-18 20:29:50 +0800

    some css & fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,5 @@
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory("src");
 	eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
-    
+    eleventyConfig.addPassthroughCopy("static");
 };
