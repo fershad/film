e45a6a0b86c6b23ec4eae97e616153a7223e8b0a
fershad <27988517+fershad@users.noreply.github.com>
2024-10-18 22:39:19 +0800

    add default title

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,10 @@
 const environment = process.env.ELEVENTY_RUN_MODE;
 
-export const site = {
-    title: "My Eleventy Site",
+const site = {
+    title: "11ty Starter Gallery",
     description: "A description of my site",
     lang: "en",
     baseUrl: environment === "serve" ? "http://localhost:8080" : "https://example.com",
 };
+
+export default site;
