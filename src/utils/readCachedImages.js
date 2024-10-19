170b5c9083f494e25bb54f9fe596edfd67ffd7bd
fershad <27988517+fershad@users.noreply.github.com>
2024-10-19 15:57:16 +0800

    change cache folder name

--- src/utils/readCachedImages.js
+++ src/utils/readCachedImages.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "node:path";
 const __dirname = import.meta.dirname;
 
-const discoveredImagesFile = await fs.readFile(path.resolve(__dirname, "../../.cache/discovered-images.json"), "utf-8");
+const discoveredImagesFile = await fs.readFile(path.resolve(__dirname, "../../_cache/discovered-images.json"), "utf-8");
 const discoveredImages = JSON.parse(discoveredImagesFile);
 
 export default discoveredImages;
