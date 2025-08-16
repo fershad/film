936a956ed39ba3e4c7769c2a5bdcaf4a1215c319
fershad <27988517+fershad@users.noreply.github.com>
2025-08-16 18:15:12 +0800

    add featured images

--- src/_data/images.js
+++ src/_data/images.js
@@ -1,3 +1,12 @@
 import discoveredImages from "../utils/readCachedImages.js";
 
+// Go through the discoveredImages and randomly assign a few images with a featured flag
+const featuredImages = discoveredImages.filter((image) => Math.random() < 0.08);
+const featuredImageIds = featuredImages.map((image) => image.file);
+
+// Assign featured flag to images
+discoveredImages.forEach((image) => {
+  image.featured = featuredImageIds.includes(image.file);
+});
+
 export default discoveredImages;
