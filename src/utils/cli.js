33a63ede9578634c36359f7cf92ba4e3f579e11d
fershad <27988517+fershad@users.noreply.github.com>
2025-08-17 11:30:16 +0800

    update cli

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -7,17 +7,17 @@ const { metadata } = config;
 
 // Define questions for metadata
 const questions = [
-    ...Object.values(metadata).map((obj) => {
+  ...Object.values(metadata).map((obj) => {
     return obj;
-  })]
-
+  }),
+];
 
 // Main function to run the CLI
 const runCLI = async (imagePath) => {
-    console.log(await terminalImage.file(imagePath, {width: "30%"}));
+  console.log(await terminalImage.file(imagePath, { width: "60%" }));
   const answers = await inquirer.prompt(questions);
-    console.log({answers})
-    return answers;
+  console.log({ answers });
+  return answers;
 };
 
 export default runCLI;
